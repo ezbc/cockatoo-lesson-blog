93b394f8dc910001281967ea44609b7fc55737c4
Elijah Bernstein-Cooper <ebernstein-cooper@lexile.com>
2023-01-23 18:50:02 -0500

    init app

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Blog from "./Blog.jsx";
+
+function App() {
+
+  return (
+    <div className="App">
+        <h1>Cockatoo Lesson Blog</h1>
+        <Blog>
+        </Blog>
+    </div>
+  )
+}
+
+export default App
---
+++ src/Blog.jsx
@@ -0,0 +1,7 @@
+export default function Blog() {
+    return <ul>
+        <li>Lesson 1.1 Project setup</li>
+        <li>Lesson 1.2 React DOM and components</li>
+        <li>Lesson 1.3 Props, state and callbacks</li>
+    </ul>
+}
