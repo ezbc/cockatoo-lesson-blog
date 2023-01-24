0cd37a1bdcfc8916c63ef01970f1eed880248157
Elijah Bernstein-Cooper <ebernstein-cooper@lexile.com>
2023-01-23 21:17:26 -0500

    moved addblogtitle to its own file

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,7 @@
 import BlogTitles from './BlogTitles.jsx';
 import Lesson1_3 from './Lesson1_3';
 import react from 'react';
-
-function AddBlogTitle(props) {
-
-    function onSubmit(event) {
-        event.preventDefault()
-        const newBlogTitle = event.target[0].value
-        props.setNewTitle(newBlogTitle)
-    }
-
-    return (
-        <form onSubmit={onSubmit}>
-            <label htmlFor={'blog-title'}>Add blog title</label>
-            <input type='text' id={'blog-title'} name={'blog-title'}></input>
-            <button type={'submit'}>Submit</button>
-        </form>
-
-    )
-
-}
-// props - pass information down tree
-// callbacks - pass information up tree
-// state -
+import AddBlogTitle from './AddBlogTitle.jsx'
 
 function App() {
     const [newTitle, setNewTitle] = react.useState();
@@ -33,7 +12,7 @@ function App() {
             <h1>Cockatoo Lesson Blog Titles
             </h1>
             <AddBlogTitle setNewTitle={setNewTitle}/>
-            <BlogTitles newTitle={newTitle} />
+            <BlogTitles newTitle={newTitle}/>
         </div>
     );
 }
