93ee3acb2dbceef8f924bdc8a072172b2f1edf85
Elijah Bernstein-Cooper <ebernstein-cooper@lexile.com>
2023-01-23 21:15:08 -0500

    lesson-1-3 completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,41 @@
-import BlogTitles from "./BlogTitles.jsx";
-import Lesson1_3 from "./Lesson1_3";
+import BlogTitles from './BlogTitles.jsx';
+import Lesson1_3 from './Lesson1_3';
+import react from 'react';
+
+function AddBlogTitle(props) {
+
+    function onSubmit(event) {
+        event.preventDefault()
+        const newBlogTitle = event.target[0].value
+        props.setNewTitle(newBlogTitle)
+    }
+
+    return (
+        <form onSubmit={onSubmit}>
+            <label htmlFor={'blog-title'}>Add blog title</label>
+            <input type='text' id={'blog-title'} name={'blog-title'}></input>
+            <button type={'submit'}>Submit</button>
+        </form>
+
+    )
+
+}
+// props - pass information down tree
+// callbacks - pass information up tree
+// state -
 
 function App() {
-
-  return (
-    <div className="App">
-        <h1>Lesson</h1>
-        <Lesson1_3/>
-        <h1>Cockatoo Lesson Blog</h1>
-        <BlogTitles>
-        </BlogTitles>
-    </div>
-  )
+    const [newTitle, setNewTitle] = react.useState();
+    return (
+        <div className="App">
+            {/*<h1>Lesson</h1>*/}
+            {/*<Lesson1_3/>*/}
+            <h1>Cockatoo Lesson Blog Titles
+            </h1>
+            <AddBlogTitle setNewTitle={setNewTitle}/>
+            <BlogTitles newTitle={newTitle} />
+        </div>
+    );
 }
 
-export default App
+export default App;
--- src/BlogTitles.jsx
+++ src/BlogTitles.jsx
@@ -1,7 +1,10 @@
-export default function BlogTitles() {
-    return <ul>
-        <li>Lesson 1.1 Project setup</li>
-        <li>Lesson 1.2 React DOM and components</li>
-        <li>Lesson 1.3 Props, state and callbacks</li>
+export default function BlogTitles(props) {
+  return (
+    <ul>
+      <li>Lesson 1.1 Project setup</li>
+      <li>Lesson 1.2 React DOM and components</li>
+      <li>Lesson 1.3 Form input, props, state and callbacks</li>
+        <li>{props.newTitle}</li>
     </ul>
+  );
 }
