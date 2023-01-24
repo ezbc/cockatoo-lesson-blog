f413894a04a57f5792f0ebcdb1bd63b09395f800
Elijah Bernstein-Cooper <ebernstein-cooper@lexile.com>
2023-01-23 19:16:23 -0500

    started lesson 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 import BlogTitles from "./BlogTitles.jsx";
+import Lesson1_3 from "./Lesson1_3";
 
 function App() {
 
   return (
     <div className="App">
+        <h1>Lesson</h1>
+        <Lesson1_3/>
         <h1>Cockatoo Lesson Blog</h1>
         <BlogTitles>
         </BlogTitles>
