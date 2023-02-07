796d8387f7c09738dd8ab738f649b4a08fb55e0d
Elijah Bernstein-Cooper <ebernstein-cooper@lexile.com>
2023-02-06 21:13:29 -0500

    delay on search

--- src/Search.jsx
+++ src/Search.jsx
@@ -1,21 +1,27 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 export default function Search({onSearch}) {
+    const [timer, setTimer] = useState(null);
 
     function onChange(event) {
-        // when this callback was handling the form onSubmit, we got a form submit event
-        // no this callback is handling the input onChange, we get a input change event
-        const searchInput = event.target.value
-        onSearch(searchInput); // on search is the callback
+        if (timer) {
+            clearTimeout(timer);
+            setTimer(null);
+        }
+        setTimer(
+            setTimeout(() => {
+                const searchInput = event.target.value
+                onSearch(searchInput); // on search is the callback
+            }, 500)
+        );
     }
 
-    return (
-        <form>
-            <label>Search Text</label>
-            <input onChange={onChange} id='search' type='text'></input>
-        </form>
-    )
-};
+return (
+    <form>
+        <label>Search Text</label>
+        <input onChange={onChange} id='search' type='text'></input>
+    </form>
+)};
 
 
 
