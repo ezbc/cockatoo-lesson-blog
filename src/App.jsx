386584648c7e905112a42b85050ceb226368ecd5
Elijah Bernstein-Cooper <ebernstein-cooper@lexile.com>
2023-02-15 06:21:35 -0500

    finished effects

--- src/App.jsx
+++ src/App.jsx
@@ -11,12 +11,29 @@ function App() {
         "Lesson 1.4 lifting state, controlled components, props handling",
         "Lesson 1.5 side effects, custom hooks, fragments",
     ]
-    const [blogTitles, setBlogTitles] = react.useState(initialTitles);
+    const blogTitlesFromLocalStorage = JSON.parse(localStorage.getItem('savedBlogTitles')) || initialTitles
+    const [blogTitles, setBlogTitles] = react.useState(blogTitlesFromLocalStorage); // replace initial with localstorage read
+    // goal is save and read blogTitles with localStorage
+    // we can use localStorage.setItem to save blogTitles - Done
+    // we can use localStorage.getItem to read blogTitles
     const [searchText, setSearchText] = react.useState('');
+
+    // run side effect when blogTitles state changes and save to localstorage
+    react.useEffect(() => {
+        localStorage.setItem('savedBlogTitles', JSON.stringify(blogTitles))
+    }, [blogTitles])
+
+    // run side effect on component render to load initial localstorage
+    // react.useEffect(() => {
+    //     setBlogTitles(JSON.parse(localStorage.getItem('savedBlogTitles')))
+    // }, [])
+
     return (
         <div>
             <h1>Cockatoo Lesson Blog Title</h1>
-            <AddBlogTitle onAddTitle={(newTitle) => setBlogTitles([...blogTitles, newTitle])}/>
+            <AddBlogTitle onAddTitle={(newTitle) => {
+                setBlogTitles([...blogTitles, newTitle]) // react does state update in background, asynchronously
+            }}/>
             <Search onSearch={setSearchText}/>
             <BlogTitles titles={blogTitles} searchText={searchText}/>
             {/*<Lessons/>*/}
