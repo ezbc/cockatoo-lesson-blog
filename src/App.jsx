5381aef6ff3e557288ff48ff4a064297cd06246b
Elijah Bernstein-Cooper <ebernstein-cooper@lexile.com>
2023-02-15 06:23:02 -0500

    removed comments

--- src/App.jsx
+++ src/App.jsx
@@ -8,25 +8,17 @@ function App() {
     const initialTitles = ["Lesson 1.1 Project setup",
         "Lesson 1.2 React DOM and components",
         "Lesson 1.3 Form input, props, state and callbacks",
-        "Lesson 1.4 lifting state, controlled components, props handling",
-        "Lesson 1.5 side effects, custom hooks, fragments",
+        "Lesson 1.4 Lifting state, controlled components, props handling",
+        "Lesson 1.5 Side effects, custom hooks, fragments",
     ]
     const blogTitlesFromLocalStorage = JSON.parse(localStorage.getItem('savedBlogTitles')) || initialTitles
-    const [blogTitles, setBlogTitles] = react.useState(blogTitlesFromLocalStorage); // replace initial with localstorage read
-    // goal is save and read blogTitles with localStorage
-    // we can use localStorage.setItem to save blogTitles - Done
-    // we can use localStorage.getItem to read blogTitles
+    const [blogTitles, setBlogTitles] = react.useState(blogTitlesFromLocalStorage);
     const [searchText, setSearchText] = react.useState('');
 
     // run side effect when blogTitles state changes and save to localstorage
     react.useEffect(() => {
         localStorage.setItem('savedBlogTitles', JSON.stringify(blogTitles))
     }, [blogTitles])
-
-    // run side effect on component render to load initial localstorage
-    // react.useEffect(() => {
-    //     setBlogTitles(JSON.parse(localStorage.getItem('savedBlogTitles')))
-    // }, [])
 
     return (
         <div>
@@ -36,7 +28,6 @@ function App() {
             }}/>
             <Search onSearch={setSearchText}/>
             <BlogTitles titles={blogTitles} searchText={searchText}/>
-            {/*<Lessons/>*/}
         </div>
     );
 }
