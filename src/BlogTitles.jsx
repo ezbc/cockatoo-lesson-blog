031bd3a1a9e43347d3ea4594ba195bb11591c0ef
Elijah Bernstein-Cooper <ebernstein-cooper@lexile.com>
2023-03-08 16:39:21 -0500

    finished reducer

--- src/BlogTitles.jsx
+++ src/BlogTitles.jsx
@@ -1,14 +1,9 @@
-export default function BlogTitles({titles, searchText}) {
-
-    function matchesSearch(blogTitle) {
-        return blogTitle.title.toLowerCase().includes(searchText.toLowerCase())
-    }
-
-    const blogsMatchingSearch = titles.filter(matchesSearch)
-
-    return (
-        <ul>
-            {blogsMatchingSearch.map((blog) => <li key={blog.id}>{blog.title}</li>)}
-        </ul>
-    );
+export default function BlogTitles({ titles }) {
+  return (
+    <ul>
+      {titles.map(blog => (
+        <li key={blog.id}>{blog.title}</li>
+      ))}
+    </ul>
+  );
 }
