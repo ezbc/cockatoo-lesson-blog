fe7d0dac3541de05895de3928846860ef5d71469
Elijah Bernstein-Cooper <ebernstein-cooper@lexile.com>
2023-03-27 21:10:58 -0400

    reset search

--- src/Search.jsx
+++ src/Search.jsx
@@ -1,42 +1,39 @@
-import React, { useState } from 'react';
-import InputAndLabel from './InputAndLabel.jsx';
+import React, {useState} from 'react';
+import InputAndLabel from './InputAndLabel.jsx'
 
-export default function Search({ onSearch, focus }) {
-  const [timer, setTimer] = useState(null);
+export default function Search({onSearch, focus}) {
+    const [timer, setTimer] = useState(null);
 
-  function onChange(event) {
-    if (timer) {
-      clearTimeout(timer);
-      setTimer(null);
+    function onChange(event) {
+        if (timer) {
+            clearTimeout(timer);
+            setTimer(null);
+        }
+        setTimer(
+            setTimeout(() => {
+                const searchInput = event.target.value
+                onSearch(searchInput);
+            }, 500)
+        );
     }
-    setTimer(
-      setTimeout(() => {
-        const searchInput = event.target.value;
-        onSearch(searchInput);
-      }, 500)
-    );
-  }
 
-  // When is passing JSX in prop appropriate
-  return (
-    <form>
-      <InputAndLabel
-        title={'Search'}
-        placeholder={'Search for blogs'}
-        id={'search'}
-        name={'search'}
-        type={'text'}
-        onChange={onChange}
-        focus={focus}
-      >
-        Search
-      </InputAndLabel>
-    </form>
-  );
-}
+    // When is passing JSX in prop appropriate
+    return (
+        <form>
+            <InputAndLabel
+                title={'Search'}
+                placeholder={'Search for blogs'}
+                id={'search'}
+                name={'search'}
+                type={'text'}
+                onChange={onChange}
+                focus={focus}
+            >
+                Search
+            </InputAndLabel>
+        </form>
+    )
+};
 
-class SearchClass extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-}
+
+
