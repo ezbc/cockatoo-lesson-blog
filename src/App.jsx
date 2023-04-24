acc17069b3ea7639dd2fe01d1c580bc0c09cd9e8
Elijah Bernstein-Cooper <ebernstein-cooper@lexile.com>
2023-04-24 18:26:59 -0400

    state in context

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,20 @@
-import { createContext } from 'react';
+import { createContext, useContext, useReducer } from 'react';
 import AppInContext from '@root/AppInContext.jsx';
+import { initialState, stateManagementFunction } from '@root/blogState.js';
 const AppContext = createContext();
 
 function App() {
+  const [state, runAction] = useReducer(stateManagementFunction, initialState);
+
   return (
-    <AppContext.Provider value={{ username: 'cookie 🍪' }}>
+    <AppContext.Provider value={{ username: 'cookie 🍪', state, runAction }}>
       <AppInContext />
     </AppContext.Provider>
   );
 }
 
+const useAppContext = () => useContext(AppContext);
+
 export default App;
 
-export { AppContext };
+export { AppContext, useAppContext };
